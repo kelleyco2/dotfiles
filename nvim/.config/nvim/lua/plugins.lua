e6a1228cd6317ed984b3f8190eb7a793789652ee
Cooper Kelley <cooper.kelley@pdq.com>
2023-08-01 14:09:56 -0600

    nvim-surround

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -90,6 +90,16 @@ return require('packer').startup(function(use)
 		requires = { 'nvim-treesitter/nvim-treesitter-textobjects' }
 	}
 
+	use {
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+	}
+
 
 	if packer_bootstrap then
     require('packer').sync()
